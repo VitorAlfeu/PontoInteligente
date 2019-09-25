487f219ab990a4f738958c4068facf7db1e982c5
velioterio <velioterio@dxc.com>
2019-09-25 13:19:45 -0300

    Commitando até o JUnit test

--- src/test/java/com/kazale/pontointeligente/api/PontoInteligenteApplicationTests.java
+++ src/test/java/com/kazale/pontointeligente/api/PontoInteligenteApplicationTests.java
@@ -3,10 +3,12 @@ package com.kazale.pontointeligente.api;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ActiveProfiles("test") //Ativando um profile do tipo test (aquele que está em application-test.properties)
 public class PontoInteligenteApplicationTests {
 
 	@Test
